f449403b24eabf2c34b9dfa950838ad1aa8357b3
McMaco <david.mccvic@educa.jcyl.es>
2019-02-28 17:07:06 +0100

    VERSION 0.0

---
+++ src/Ficheros.java
@@ -0,0 +1,14 @@
+/***PROGRAMA DE FICEHROS
+ * AUTOR: DAVID MCCONNELL VICENTE
+ * FECHA: 28/02/2019
+ * CURSO: DM1B
+ */
+
+import java.util.*;
+
+public class Ficheros {
+	
+	public static void main(String [] args) {
+		//NOMBRE Y APELLIDOS, DIRECCION, C.P, LOCALIDAD, PROVINCIA, TELEFONO, EDAD
+	}
+}
